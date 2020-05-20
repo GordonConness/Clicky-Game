b124c52485157437857ede6aebf3b5ac7606074c
Gordon Conness <gordon.conness1@gmail.com>
2020-05-20 13:37:52 -0500

    praying

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById("root"));
-serviceWorker.unregister();
-
+//serviceWorker.unregister();
+registerServiceWorker();
