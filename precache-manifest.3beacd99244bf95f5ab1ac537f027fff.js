75a5a97143521fb731cb6a062a1d4c3c830d7a42
Gordon Conness <gordon.conness1@gmail.com>
2020-05-20 18:09:17 -0500

    Updates

---
+++ precache-manifest.3beacd99244bf95f5ab1ac537f027fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0dc0fa1713a3f302a2f0919b452660",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87806bc16487f13dc785",
+    "url": "/static/css/main.a224fa2d.chunk.css"
+  },
+  {
+    "revision": "25edf8911c86ff417e68",
+    "url": "/static/js/2.666e6ab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.666e6ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87806bc16487f13dc785",
+    "url": "/static/js/main.6c728411.chunk.js"
+  },
+  {
+    "revision": "028583e914e2b8d1768c",
+    "url": "/static/js/runtime-main.c4582c88.js"
+  }
+]);
