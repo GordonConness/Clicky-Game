36532104973957ad9aa23c52fe84f163885d3ebf
Gordon Conness <gordon.conness1@gmail.com>
2020-05-26 20:57:44 -0500

    final adjustments to pictures

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Score = props => (
 <div
     className="gameScore">
-        <h4 className="score">Your Score{props.total}</h4>
+        <h4 className="score">Your Score: {props.total}</h4>
         <h4 className="status">{props.status}</h4>
 </div>
 );
